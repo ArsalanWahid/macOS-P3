8a80875c886d907d9de0fd27246e81ac75857806
arsalanwahid <arslanwahid1993@gmail.com>
2021-01-08 00:52:44 +0500

    Setup toolbar and made IBActions for browser controls

--- Grid View Browser/Grid View Browser/ViewController.swift
+++ Grid View Browser/Grid View Browser/ViewController.swift
@@ -20,7 +20,21 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
-
-
+    
+    @IBAction func urlEntered(_ sender: NSTextField) {
+        print(sender.stringValue)
+    }
+    
+    @IBAction func navigationClicked(_ sender: NSTextField) {
+        print("navigationClicked")
+    }
+    
+    @IBAction func adjustRows(_ sender: NSTextField) {
+        print("adjustRows")
+    }
+    
+    @IBAction func adjustColumns(_ sender: NSTextField) {
+        print("adjustColumns")
+    }
 }
 
