5915d34064cc45feb3b2c71d59c1d69650659d03
arsalanwahid <arslanwahid1993@gmail.com>
2021-01-08 01:08:30 +0500

    Added WkWebView to stack view with layout constrains by code only

--- Grid View Browser/Grid View Browser/ViewController.swift
+++ Grid View Browser/Grid View Browser/ViewController.swift
@@ -6,18 +6,36 @@
 //
 
 import Cocoa
+import WebKit
 
-class ViewController: NSViewController {
-
+class ViewController: NSViewController,WKNavigationDelegate {
+    
+    var rows:NSStackView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+//      Make a vertical stack view and add it to view
+        rows = NSStackView()
+        rows.orientation = .vertical
+        rows.distribution = .fillEqually
+        rows.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(rows)
+        
+//        create autolayout constrains the pin stackview to the edges of its container (superview)
+        rows.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        rows.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        rows.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        rows.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        
+//      create initial column that contains a single webview
+        let column = NSStackView(views: [makeWebView()])
+        column.distribution = .fillEqually
+//
+        rows.addArrangedSubview(column)
     }
-
+    
     override var representedObject: Any? {
         didSet {
-        // Update the view, if already loaded.
+            
         }
     }
     
@@ -35,6 +53,15 @@ class ViewController: NSViewController {
     
     @IBAction func adjustColumns(_ sender: NSTextField) {
         print("adjustColumns")
+    }
+    
+    //MARK:- Methods
+    func makeWebView() -> NSView {
+        let webView = WKWebView()
+        webView.navigationDelegate = self
+        webView.wantsLayer = true
+        webView.load(URLRequest(url: URL(string: "https://www.apple.com")!))
+        return webView
     }
 }
 
