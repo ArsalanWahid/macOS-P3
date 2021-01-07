b4d59f7dc010ec0111f71bda6bf3ac19e466767e
arsalanwahid <arslanwahid1993@gmail.com>
2021-01-07 23:28:53 +0500

    Initial commit

---
+++ Grid View Browser/Grid View Browser/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Grid View Browser
+//
+//  Created by Arsalan Wahid Asghar on 07/01/2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
