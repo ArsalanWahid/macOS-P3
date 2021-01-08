57c3efb6c3c4f04088891e66dcd7171e20ac498e
arsalanwahid <arslanwahid1993@gmail.com>
2021-01-08 20:37:50 +0500

    Added logic to add/remove row/columns of browsers

--- Grid View Browser/Grid View Browser/ViewController.swift
+++ Grid View Browser/Grid View Browser/ViewController.swift
@@ -13,23 +13,27 @@ class ViewController: NSViewController,WKNavigationDelegate {
     var rows:NSStackView!
     override func viewDidLoad() {
         super.viewDidLoad()
-//      Make a vertical stack view and add it to view
+        //      Make a vertical stack view and add it to view
         rows = NSStackView()
+        //        view will be stacks from top to bottom
         rows.orientation = .vertical
         rows.distribution = .fillEqually
+        //        need to set false to allow custom layout constrains by code
         rows.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(rows)
         
-//        create autolayout constrains the pin stackview to the edges of its container (superview)
+        //        create autolayout constrains the pin stackview to the edges of its container (superview)
+        //        Anchors are simplified layers over Auto Layout
         rows.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
         rows.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
         rows.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
         rows.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
         
-//      create initial column that contains a single webview
+        //      create another stackview by default its orientation is horizontal
+        //        will be stacked left to right
         let column = NSStackView(views: [makeWebView()])
         column.distribution = .fillEqually
-//
+        //
         rows.addArrangedSubview(column)
     }
     
@@ -47,12 +51,61 @@ class ViewController: NSViewController,WKNavigationDelegate {
         print("navigationClicked")
     }
     
-    @IBAction func adjustRows(_ sender: NSTextField) {
-        print("adjustRows")
+    @IBAction func adjustRows(_ sender: NSSegmentedControl) {
+        if sender.selectedSegment == 0 {
+            //To add a row
+            //            count how many columns are in the first row so far
+            let colCount = (rows.arrangedSubviews[0] as! NSStackView).arrangedSubviews.count
+            
+            //make number of webviews based on existing cols in the first row
+            let viewArray = (0 ..< colCount).map {_ in makeWebView()}
+            
+            //            use array to make a new stack view
+            let row = NSStackView(views: viewArray)
+            row.distribution = .fillEqually
+            rows.addArrangedSubview(row)
+        }else {
+            //            deleting rows
+            //            make sure more than one row exists
+            guard rows.arrangedSubviews.count > 1 else { return }
+            // pull out the last row
+            guard let rowToRemove = rows.arrangedSubviews.last as? NSStackView else { return }
+            
+            //Loop each col and remove the webview
+            for col in rowToRemove.arrangedSubviews {
+                col.removeFromSuperview()
+            }
+            
+            //Now remove the stackview
+            rows.removeArrangedSubview(rowToRemove)
+        }
     }
     
-    @IBAction func adjustColumns(_ sender: NSTextField) {
-        print("adjustColumns")
+    @IBAction func adjustColumns(_ sender: NSSegmentedControl) {
+        if sender.selectedSegment == 0 {
+            //            Need to add a column
+            for case let row as NSStackView in rows.arrangedSubviews {
+                row.addArrangedSubview(makeWebView())
+            }
+        }else {
+            //To delete a column
+            
+            //pull out the first row
+            guard let firstRow = rows.arrangedSubviews.first as? NSStackView else { return }
+            
+            //make sure this row has two columns
+            guard firstRow.arrangedSubviews.count > 1 else { return }
+            
+            //if conditions pass we are safe to delete a column
+            for case let row as NSStackView in rows.arrangedSubviews {
+                //loop over every row
+                if let last = row.arrangedSubviews.last {
+                    //remove the column using the two step process
+                    row.removeArrangedSubview(last)
+                    last.removeFromSuperview()
+                }
+            }
+        }
     }
     
     //MARK:- Methods
